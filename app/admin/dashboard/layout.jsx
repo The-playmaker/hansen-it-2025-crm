8b747293fb600f35967ec27c291675c7107e5076
The-playmaker <123890085+The-playmaker@users.noreply.github.com>
2026-01-18 22:34:26 +0100

    Create layout.jsx

---
+++ app/admin/dashboard/layout.jsx
@@ -0,0 +1,20 @@
+Error occurred prerendering page "/admin/services". Read more: https://nextjs.org/docs/messages/prerender-error
+ReferenceError: AdminLayout is not defined
+    at p (/vercel/path0/.next/server/app/admin/services/page.js:1:3603)
+    at nj (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:46251)
+    at nM (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:47571)
+    at nN (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:64546)
+    at nI (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:47010)
+    at nM (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:47717)
+    at nM (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:61546)
+    at nN (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:64546)
+    at nB (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:67538)
+    at nD (/vercel/path0/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:12:66680)
+   Generating static pages (20/27) 
+ ✓ Generating static pages (27/27)
+> Export encountered errors on following paths:
+	/admin/calendar/page: /admin/calendar
+	/admin/employees/page: /admin/employees
+	/admin/kanban/page: /admin/kanban
+	/admin/services/page: /admin/services
+Error: Command "npm run build" exited with 1
